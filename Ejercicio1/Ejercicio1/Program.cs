54bffdae9913356c44f3f0cc4a81a05f751f5998
josue052015 <76499684+josue052015@users.noreply.github.com>
2021-06-18 07:57:04 +0200

    Initial commit

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //interfaz
+            var saludoMensaje = new EnviarSaludo();
+            InvocarSaludo(saludoMensaje,"PRUEBA");
+
+            //parametros de tipo
+            var parametros = new ParameterType<int, string>();
+            parametros.PropertyOne = 1;
+            parametros.PropertyTwo = "PropertyTwo";
+            Console.WriteLine(parametros.PropertyOne +" "+ parametros.PropertyTwo);
+
+            //clase base
+            rayoMcQueen2 E = new rayoMcQueen2();
+            E.sayProperty();
+
+            //enum
+            var numerosEnum1 = numeros.num1;
+            var numerosEnum2 = numeros.num2;
+
+            //valor nullable
+            int? NullableInt = 1;
+
+            Console.Read();
+        }
+
+        public static void InvocarSaludo(ISaludo mensaje, string contenidoSaludo)
+        {
+            mensaje.MensajeSaludo(contenidoSaludo);
+        }
+
+    }
+    class EnviarSaludo: ISaludo
+    {
+        public void MensajeSaludo(string saludo)
+        {
+            Console.WriteLine("Saludos");
+        }
+    }
+    interface ISaludo
+    {
+        void MensajeSaludo(string contenidoSaludo);
+    }
+
+    /******************PARAMETROS DE TIPO******************************/
+    public class ParameterType<one,two>
+    {
+        public one PropertyOne { get; set; }
+        public two PropertyTwo { get; set; }
+    }
+
+    /*********************CLASES BASE*******************************/
+    public class rayoMcQueen
+    {
+       
+        public string color = "rojo";
+
+        public virtual void sayProperty()
+        {
+            Console.WriteLine("Color: {0}", color);
+            
+        }
+    }
+    class rayoMcQueen2 : rayoMcQueen
+    {
+        public string frase = "qchao";
+        public override void sayProperty()
+        {
+            
+            base.sayProperty();
+            Console.WriteLine("Frase: {0}", frase);
+        }
+    }
+    /***************ENUM***********************/
+    [Flags]
+    public enum numeros
+    {
+        num1 = 1,
+        num2 = 2
+
+    }
+    
+
+}
---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**********PROPIEDADES**********/
+
+            var Carros = new List<string>();
+            Carros.Capacity = 100;
+            Carros.Add("Mercedez");
+            int Counter = Carros.Count;
+            Console.WriteLine("La cuenta de la lista es: " + Counter);
+
+            /*******PARAMETRO DE REFERENCIA*******/
+            string Value = "Soy un valor de referencia";
+            referenceMethod(ref Value);
+            Console.WriteLine(Value);
+
+            //resultado del outparameter
+            ShowSuma();
+
+            // array parameter
+            int[] numberList = { 5, 6, 7, 8, 9 };
+            arrayParameters(numberList);
+
+            Console.Read();
+
+        }
+        /**************ARRAY PARAMETER CON FOR y metodo protected*****************/
+        protected static void arrayParameters(params int[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.Write(list[i] + " - ");
+            }
+
+        }
+
+        static void referenceMethod(ref string refParameter)
+        {
+            refParameter = refParameter + " " + "ha sido referenciado";
+        }
+
+        /******OUTPUT PARAMETERS***************/
+
+       public static void suma(int num1,int num2, out int result)
+        {
+           
+            result = num1 + num2;
+        }
+
+       public static void ShowSuma()
+        {
+            suma(1,6, out int res);
+            Console.WriteLine("El resultado de la suma es: " + res);
+        }
+
+       
+
+    }
+
+    /*********************Abstract method*****************/
+
+    public abstract class music
+    {
+        public abstract string play(string Lyric);
+    }
+
+    public class Regaetton : music
+    {
+        string _lyric;
+        public Regaetton(string lyric) => _lyric = lyric;
+        public override string play(string lyric)
+        {
+            return "Sabes que somos de calle";
+        }
+    }
+    /****************FIELD****************/
+    public class colores
+    {
+        public string color1;
+        public string color2;
+
+        private colores(string primerColor, string segundoColor)
+        {
+            color1 = primerColor;
+            color2 = segundoColor;
+        }
+
+        /***********METODO SIMPLE****************/
+        public int MetodoSimple() => 1;
+
+        
+    }
+
+    public class buclesYcondiciones
+    {
+       
+       public static void WHILE()
+        {
+            int Num = 0;
+            int Final = 5;
+            while(Num <= Final)
+            {
+                Console.WriteLine("sigo estando en el while");
+            }
+        }
+
+        public static void IF()
+        {
+            int number1 = 20;
+            int Number2 = 15;
+
+            if(number1 < Number2)
+            {
+                Console.WriteLine("soy menor");
+            }else if (number1 == Number2)
+            {
+                Console.WriteLine("soy igual");
+            }
+            else
+            {
+                Console.WriteLine("soy mayor");
+            }
+        }
+        
+    }
+
+    /****************VIRTUAL Y OVERRIDED METHOD********************/
+    public class rayoMcQueen
+    {
+
+        public string color = "rojo";
+
+        public virtual void sayProperty()
+        {
+            Console.WriteLine("Color: {0}", color);
+
+        }
+    }
+    class rayoMcQueen2 : rayoMcQueen
+    {
+        public string frase = "qchao";
+        public override void sayProperty()
+        {
+
+            base.sayProperty();
+            Console.WriteLine("Frase: {0}", frase);
+        }
+    }
+    
+    /***************************Method overloading**************************/
+    public class overload
+    {
+        static void Value() => Console.WriteLine("Exito");
+        static void Value(int x) => Console.WriteLine("Value(int)");
+        public static void Use()
+        {
+            Value();
+            Value(76);
+        }
+    }
+   
+
+}
