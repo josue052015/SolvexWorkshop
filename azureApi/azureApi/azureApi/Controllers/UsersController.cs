fbf465cd45e443840cef6f738bc732a467f03f71
josue052015 <76499684+josue052015@users.noreply.github.com>
2021-08-11 07:28:51 +0200

    10-08-21 azure api

---
+++ azureApi/azureApi/azureApi/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using azureApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace azureApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IAuthenticationProvider _authenticationProvider;
+        private readonly UserService _service;
+
+        public UsersController(IAuthenticationProvider authenticationProvider, UserService service)
+        {
+            _authenticationProvider = authenticationProvider;
+            this._service = service;
+        }
+        [HttpGet]
+        public async Task<List<User>> GetUser()
+        {
+            //var sc = new GraphServiceClient(_authenticationProvider);
+            //var users = await sc.Users.Request().GetAsync();
+            //return users.ToList();
+            var result = await _service.GetUsers();
+            return (List<User>)result;
+
+
+        }
+    }
+}
---
+++ azureApi/azureApi/azureApi/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using azureApi.Entities;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace azureApi.Services
+{
+    public class UserService
+    {
+        public async Task<ICollection<Users>> GetUsers()
+        {
+            IConfidentialClientApplication clientApplication = ConfidentialClientApplicationBuilder
+                          .Create("ea56df0b-11e7-404e-b32a-3ee8c18bf101")
+                          .WithTenantId("f8cdef31-a31e-4b4a-93e4-5f571e91255a")
+                          .WithClientSecret("6rW.3X51mKoyHpK_uf2W~K1YroyFF.NFk2")
+                           .Build();
+
+            ClientCredentialProvider authProvider = new ClientCredentialProvider(clientApplication);
+          //  ClientCredential authProvider = new ClientCredential("ea56df0b-11e7-404e-b32a-3ee8c18bf101", "6rW.3X51mKoyHpK_uf2W~K1YroyFF.NFk2");
+            GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+
+            var responseData = await graphClient
+                .Users
+                .Request()
+                .GetAsync();
+            return (ICollection<Users>)responseData;
+        } 
+    }
+}
